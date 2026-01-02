3bb8828fde0a9f2a50ee17966e46a3e23bcb32c7
Alex_de_code <alexandergonzalez@pursuit.orgclear>
2026-01-02 17:47:16 -0500

    updated experience card to map through array of objects instead of hard coding

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,54 +1,70 @@
 import { ExternalLinkIcon } from "@radix-ui/react-icons";
 
 function Experience() {
-  return (
-    <>
-      {/* map through array of experience objects, will create conditional renders for relevant links section */}
-      <div className="grid grid-col-1 gap-1 text-gray-200 rounded-md p-3  hover:bg-white/10 hover:shadow  transition-colors duration-150">
-        <span className="flex flex-row justify-between items-center ">
-          {/* experience title */}
-          <h5 className="">Experience Title</h5>
-          {/* date range opportunity took place */}
-          <h4 className="mr-3 text-sm text-white/70">
-            {" "}
-            JANUARY 2026 - PRESENT{" "}
-          </h4>
-        </span>
-        {/* job description */}
-        <p className="text-sm">
-          Contrary to popular belief, Lorem Ipsum is not simply random text. It
+  const experiencesData = [
+    {
+      roleTitle: "People Engagement Lead",
+      companyName: "Capgemini",
+      workingPeriod: "January 2026 - Present",
+      description: `Contrary to popular belief, Lorem Ipsum is not simply random text. It
           has roots in a piece of classical Latin literature from 45 BC, making
           it over 2000 years old. Richard McClintock, a Latin professor at
           Hampden-Sydney College in Virginia, looked up one of the more obscure
           Latin words, consectetur, from a Lorem Ipsum passage, and going
           through the cites of the word in classical literature, discovered the
-          undoubtable source.
-        </p>
-        {/* relevant links list */}
-        <div className="flex flex-row mt-2">
-          {/* map urls here */}
-          <a
-            href=""
-            target="_blank"
-            className="hover:text-orange-400 flex flex-row items-center text-white"
-          >
-            <ExternalLinkIcon className="h-4 w-4 mr-1" />
-            <p className="text-sm">Link</p>
-          </a>
+          undoubtable source.`,
+      links: [{ label: "Project Demo", url: "https://example.com" }],
+      skills: ["JavaScript", "React", "NextJS"],
+    },
+  ];
+
+  return (
+    <>
+      {/* map through array of experience objects, will create conditional renders for relevant links section */}
+      {experiencesData.map((experience) => (
+        <div className="grid grid-col-1 gap-1 text-gray-200 rounded-md p-3  hover:bg-white/10 hover:shadow  transition-colors duration-150">
+          <span className="flex flex-row justify-between items-center ">
+            {/* experience title */}
+            <h5 className="">{experience.roleTitle}</h5>
+            {/* date range opportunity took place */}
+            <h4 className="mr-3 text-sm text-white/70">
+              {experience.workingPeriod}
+            </h4>
+          </span>
+          {/* job description */}
+          <p className="text-sm">{experience.description}</p>
+          {/* relevant links list */}
+          {experience.links && experience.links.length > 0 && (
+            <div className="flex flex-row mt-2">
+              {experience.links.map((link, index) => (
+                <a
+                  key={index}
+                  href={link.url}
+                  target="_blank"
+                  className="hover:text-orange-400 flex flex-row items-center text-white"
+                >
+                  <ExternalLinkIcon className="h-4 w-4 mr-1" />
+                  <p className="text-sm">{link.label}</p>
+                </a>
+              ))}
+            </div>
+          )}
+          {experience.skills && experience.skills.length > 0 && (
+            <div className="flex flex-row space-x-2 mt-2">
+              {/* skills list  */}
+              {experience.skills.map((skill, index) => (
+                <div
+                  key={index}
+                  className="inline-block px-3 py-1 rounded-full text-sm bg-orange-400/10 text-orange-400"
+                >
+                  {" "}
+                  {skill}
+                </div>
+              ))}
+            </div>
+          )}
         </div>
-        <div className="flex flex-row space-x-2 mt-2">
-          {/* skills list  */}
-          <div className="inline-block px-3 py-1 rounded-full text-sm bg-orange-400/10 text-orange-400">
-            Javascript
-          </div>
-          <div className="inline-block px-3 py-1 rounded-full text-sm bg-orange-400/10 text-orange-400">
-            React.JS
-          </div>
-          <div className="inline-block px-3 py-1 rounded-full text-sm bg-orange-400/10 text-orange-400">
-            Next.JS
-          </div>
-        </div>
-      </div>
+      ))}
     </>
   );
 }
