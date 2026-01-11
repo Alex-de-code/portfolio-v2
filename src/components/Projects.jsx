c98aadda74799a814fff3037b1e601b92433166a
Alex_de_code <alexandergonzalez@pursuit.orgclear>
2026-01-10 19:30:42 -0500

    created projectsData.js + setup json structure + mapped to Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,39 +1,55 @@
+import { projectsData } from "../data/projects.js";
 import Wave from "../assets/wave-3.png";
+import { ExternalLinkIcon, VideoIcon } from "@radix-ui/react-icons";
 
 const Projects = () => {
   return (
     <>
       <div>
         <div>Projects</div>
-        <div className="group grid grid-cols-10 gap-1 text-gray-200 rounded-md p-3  hover:bg-white/5 hover:shadow  transition-colors duration-150">
-          <img
-            src={Wave}
-            alt="placeholder image text"
-            className="mt-1 p-1 col-span-3 sm:block h-3/5 w-full mask-radial-from-15% group-hover:mask-radial-from-90% "
-          />
-          <div className="col-span-7 ml-1">
-            <h5 className="font-medium group-hover:text-orange-400 transition-colors duration-150">
-              Project Title
-            </h5>
-            <p className="text-sm">
-              It is a long established fact that a reader will be distracted by
-              the readable content of a page when looking at its layout. The
-              point of using Lorem Ipsum is that it has a more-or-less normal
-              distribution of letters, as opposed to using 'Content here,
-              content here', making it look like readable English.
-            </p>
-            <div className="flex flex-wrap gap-2 mt-2">
-              {/* skills list  */}
-
-              <div className="inline-block px-3 py-1 rounded-full text-sm bg-orange-400/10 text-orange-400 border border-orange-400/10 shadow-2xs">
-                Test
-              </div>
-              <div className="inline-block px-3 py-1 rounded-full text-sm bg-orange-400/10 text-orange-400 border border-orange-400/10 shadow-2xs">
-                Test
-              </div>
+        {projectsData.map((project) => (
+          <div className="group grid grid-cols-10 gap-1 text-gray-200 rounded-md p-3  hover:bg-white/5 hover:shadow  transition-colors duration-150">
+            <img
+              src={Wave}
+              alt="placeholder image text"
+              className="mt-1 p-1 col-span-3 sm:block h-3/5 w-full rounded-lg mask-radial-from-15% group-hover:mask-radial-from-90% "
+            />
+            <div className="col-span-7 ml-1">
+              <h5 className="font-medium group-hover:text-orange-400 transition-colors duration-150">
+                {project.projectTitle}
+              </h5>
+              <p className="text-sm">{project.description}</p>
+              {project.links && project.links.length > 0 && (
+                <div className="flex flex-wrap gap-2">
+                  {project.links.map((link, index) => (
+                    <a
+                      href={link.url}
+                      key={index}
+                      target="_blank"
+                      className="hover:text-teal-200 flex flex-row items-center text-white"
+                    >
+                      <ExternalLinkIcon className="h-4 w-4 mr-1" />
+                      <p className="text-sm">{link.label}</p>
+                    </a>
+                  ))}
+                </div>
+              )}
+              {project.tech && project.tech.length > 0 && (
+                <div className="flex flex-wrap gap-2 mt-2">
+                  {/* Tools list  */}
+                  {project.tech.map((tool, index) => (
+                    <div
+                      key={index}
+                      className="inline-block px-3 py-1 rounded-full text-sm bg-orange-400/10 text-orange-400 border border-orange-400/10 shadow-2xs"
+                    >
+                      {tool}
+                    </div>
+                  ))}
+                </div>
+              )}
             </div>
           </div>
-        </div>
+        ))}
       </div>
     </>
   );
---
+++ src/data/projects.js
@@ -0,0 +1,17 @@
+export const projectsData = [
+  {
+    projectTitle: "Connections Bot",
+    description: `Automated contact management system that sends weekly Telegram reminders and updates Google Sheets with one click. Perfect for busy people who still want to be intentional with their treasured relationships.`,
+    links: [
+      {
+        label: "Repo",
+        url: "https://github.com/Alex-de-code/telegram-contact-checkin-bot",
+      },
+      {
+        label: "Video Tutorials",
+        url: "https://www.loom.com/share/folder/e03d7706f93545f98f70654e86fed37e",
+      },
+    ],
+    tech: ["Pipedream", "Google Sheets API", "Telegram Bot API", "Loom"],
+  },
+];
